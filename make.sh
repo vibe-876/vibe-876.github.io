13e71693fa10d944a7634ab7c234f103b2a65d04
cam <cameronayres123@gmail.com>
2026-04-22 21:22:35 +0100

    Fixed make.sh, and wrote a blog entry about it.

--- make.sh
+++ make.sh
@@ -3,11 +3,16 @@
 
 echo "<!DOCTYPE html><head><title>Blog</title><link rel=\"stylesheet\" href=\"styles.css\"><body><h1>Blog Entries</h1><a href=\"index.html\">Back Home</a><ul>" > blog.html
 
-pages=$(find ./posts -type f -iname \*.org)
+pages=$(find ./posts -type f -iname \*.org | sort)
 for page in $pages
 do
-    pandoc $page -o .$(echo $page | cut -f2 -d'.').html --css="styles.css" -s
-    echo "<li><a href=\".$(echo $page | cut -f2 -d'.').html\">$(grep "#+title" $page | cut -f2- -d' '), $(grep "#+date" $page | cut -f2 -d' ')</a></li>" >> blog.html
+    echo "Building $page..."
+    pandoc $page -o .$(echo $page | cut -f2 -d'.').html --css="../styles.css" -s
+
+    pagename=.$(echo $page | cut -f2 -d'.').html
+    pagetitle=$(grep "#+title" $page | cut -f2- -d' ' | head -n1)
+    pagedate=$(grep "#+date" $page | cut -f2 -d' ')
+    echo "<li><a href=\"$pagename\">$pagetitle, $pagedate</a></li>" >> blog.html
 done
 
 echo "</ul></body>" >> blog.html
